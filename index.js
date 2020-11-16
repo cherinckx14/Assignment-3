e600b6b9b79da2057ecca582fb7980b971e29a4f
cherinckx14 <39385152+cherinckx14@users.noreply.github.com>
2020-11-15 23:37:41 -0800

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,43 @@
  * Write your client-side JS code in this file.  Don't forget to include your
  * name and @oregonstate.edu email address below.
  *
- * Name:
- * Email:
+ * Name: Christian Herinckx
+ * Email: herincch@oregonstate.edu
  */
+
+function add_blog_post(photoURL, caption){
+  var newPost = document.createElement('section');
+  
+  var postDiv = document.createElement('div');
+  postDiv.classList.add("post");
+  postDiv.setAttribute("data-price", price);
+  postDiv.setAttribute("data-city", city);
+  postDiv.setAttribute("data-condition", condition);
+  newPost.appendChild(postDiv);
+  
+  var postContentsDiv = document.createElement('div');
+  postContentsDiv.classList.add("post-contents");
+  postDiv.appendChild(postContentsDiv);
+  
+  var postImgDiv = document.createElement('div');
+  postImgDiv.classList.add("post-image-container");
+  postImgDiv.src = photoURL;
+  postImgDiv.alt = item;
+  postContentsDiv.appendChild(postImgDiv);
+  
+  var postInfoDiv = document.createElement('div');
+  postInfoDiv.classList.add("post-info-container");
+  postInfoDiv.textContent = caption
+  postContentsDiv.appendChild(postInfoDiv);
+  
+  
+  photoCardSection.classList.add("photo-card");
+  photoCardSection.classList.add("another-class");
+  photoCardSection.classList.add("another-class");
+  
+  var imgContainerDiv = document.createElement('div');
+  imgContainerDiv.classList.add("img-container");
+  photoCardSection.appendChild(imgContainerDiv);
+  
+  
+}
